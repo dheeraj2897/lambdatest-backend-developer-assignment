bb29345bd2aa56bf213180e4c979aaddd18bd2d9
dheeraj2897 <dheerajgupta2897@gmail.com>
2024-03-09 21:48:17 +0530

    model for enriched data

---
+++ lambda-test-backend-assignment/src/main/java/com/dheeraj/lambdatest/assignement/model/EnrichedData.java
@@ -0,0 +1,28 @@
+package com.dheeraj.lambdatest.assignement.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EnrichedData {
+	private String title;
+
+    @JsonIgnore
+    private final Map<String, Object> additionalProperties = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
+
