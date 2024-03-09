c8bde32ddc91393006438a2c9b9cc4d5c867cb20
dheeraj2897 <dheerajgupta2897@gmail.com>
2024-03-09 21:51:55 +0530

    add commandlineRunner run bean
    This will invoke the service layer method once, the application starts
    on the default port by commandLine

--- lambda-test-backend-assignment/src/main/java/com/dheeraj/lambdatest/assignement/JsonEnricherApplication.java
+++ lambda-test-backend-assignment/src/main/java/com/dheeraj/lambdatest/assignement/JsonEnricherApplication.java
@@ -1,7 +1,13 @@
 package com.dheeraj.lambdatest.assignement;
 
+import java.io.IOException;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.dheeraj.lambdatest.assignement.service.JsonEnricherService;
 
 @SpringBootApplication
 public class JsonEnricherApplication {
@@ -9,5 +15,17 @@ public class JsonEnricherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JsonEnricherApplication.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner run(JsonEnricherService jsonEnricherService) {
+        return args -> {
+            try {
+                jsonEnricherService.enrichJsonFiles();
+                System.out.println("Enrichment completed. Check the output file.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    }
 
 }
