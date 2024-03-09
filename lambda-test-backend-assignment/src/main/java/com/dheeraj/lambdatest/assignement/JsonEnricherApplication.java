730ce3b2152fb35c72acc22b4a540e52412d82f8
dheeraj2897 <dheerajgupta2897@gmail.com>
2024-03-09 14:15:23 +0530

    initial project setup
    Add boilerplate code for project

---
+++ lambda-test-backend-assignment/src/main/java/com/dheeraj/lambdatest/assignement/JsonEnricherApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.lambdatest.assignement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonEnricherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonEnricherApplication.class, args);
+	}
+
+}
